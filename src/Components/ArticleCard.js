b610942ec9d1d03efc94329b5156f4b73c375358
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2020-10-16 17:59:26 +0000

    Updated Routing, Fixed Scroll To Top on Route change

--- src/Components/ArticleCard.js
+++ src/Components/ArticleCard.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import blogImg from '../Images/placeholder.jpg';
 
 function ArticleCard() {
   return (
-    <div className="col-md-6 mb-5 blog-card">
-      <div className="card border-0">
+    <div className="col-md-6 mb-5 pb-5 blog-card">
+      <div className="card border-0 bg-transparent">
         <div className="card-img-top blog-card-img" style={{backgroundImage: `url(${blogImg})`}}>  </div>
         <div className="card-body p-0 py-3">
           <span className="post-date text-muted d-inline-block my-3">Oct 12</span>
           <h2 className="card-title font-weight-light">This guy really won't shut up</h2>
           <p className="card-text text-truncate text-wrap">Four new interviews in which our "CEO" shares how design can help the internet be less divisive, among other things.</p>
-          <a href="google.com" className="btn-link mt-3 d-inline-block"><i class="ri-arrow-drop-right-line"></i> Read Post</a>
+          <Link to="/article" className="btn-link mt-3 d-inline-block"><i className="ri-arrow-drop-right-line"></i> Read Post</Link>
         </div>
       </div>
     </div>
