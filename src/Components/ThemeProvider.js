96af08200fc35d52ce1b3609a92b242582836283
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2020-10-16 17:59:49 +0000

    Updated Routing, Fixed Scroll To Top on Route change

---
+++ src/Components/ThemeProvider.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+export const Theme = createContext("dark");
+
+function ThemeProvider({ children }) {
+
+  const [theme, setTheme] = useState("light");
+  const themeValue = [theme, setTheme];
+
+  return(
+    <Theme.Provider value={ themeValue } >
+      {children}
+    </Theme.Provider>
+  );
+}
+
+export default ThemeProvider;
+
